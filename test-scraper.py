bc9587e6951a20df3f1163884353c24b6bf8ba9c
Zakaria El hjouji <zakariaelhjouji@Zakarias-MacBook-Pro.local>
2018-12-25 16:44:47 -0800

    a

--- test-scraper.py
+++ test-scraper.py
@@ -1,5 +1,10 @@
+from pyvirtualdisplay import Display
 from selenium import webdriver
 from bs4 import BeautifulSoup
+
+display = Display(visible=0, size=(800, 600))
+display.start()
+
 
 url = 'https://www.zillow.com/user/Login.htm'
 
@@ -13,6 +18,7 @@ soup = BeautifulSoup(html, 'lxml')
 #ex_zip = [24555]
 #d_houses = sc.get_houses(driver, ex_zip)
 #print(d_houses) 
+display.stop()
 
 print(soup)
 
