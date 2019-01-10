4ae4afd4fe2cd366fc27e7b600a0fb62e22d6f45
Zakaria El hjouji <zakariaelhjouji@Zakarias-MacBook-Pro.local>
2019-01-09 22:36:24 -0800

    test

--- test-scraper.py
+++ test-scraper.py
@@ -1,41 +1,58 @@
-from pyvirtualdisplay import Display
+# from pyvirtualdisplay import Display
 from selenium import webdriver
 from bs4 import BeautifulSoup
+import scraper as sc
 
-display = Display(visible=0, size=(800, 600))
-display.start()
+# display = Display(visible=0, size=(800, 600))
+# display.start()
 
 
 url = 'https://www.zillow.com/user/Login.htm'
 
-driver = webdriver.Chrome()
+# driver = webdriver.Chrome()
 
-driver.get(url)
+# driver.get(url)
 
-html = driver.page_source
-soup = BeautifulSoup(html, 'lxml')
-print('We got the first soup')
+# html = driver.page_source
+# soup = BeautifulSoup(html, 'lxml')
+# print('We got the first soup')
 
-driver.quit()
+# driver.quit()
+
+driver = webdriver.Firefox()
+login_url = "https://www.zillow.com/user/Login.htm"
+#access the login page
+driver.get(login_url)
+username = driver.find_element_by_id("email")
+password = driver.find_element_by_id("password")
+
+email = "elhjouji.zakaria@gmail.com"
+pwd = "Samsung123" 
+username.send_keys(email)
+password.send_keys(pwd)
+login_attempt = driver.find_element_by_xpath("//*[@type='submit']")
+login_attempt.submit()
 
 
 ####
 
 
-driver = webdriver.Chrome()
+# driver = webdriver.Chrome()
 
-driver.get(url)
+# driver.get(url)
 
-html = driver.page_source
-soup = BeautifulSoup(html, 'lxml')
-print('We got the second soup')
+# html = driver.page_source
+# soup = BeautifulSoup(html, 'lxml')
+# print('We got the second soup')
 
-driver.quit()
+# driver.quit()
 
-#ex_zip = [24555]
-#d_houses = sc.get_houses(driver, ex_zip)
-#print(d_houses) 
-display.stop()
+# ex_zip = ['24555']
+
+# driver = sc.connect()
+# d_houses = sc.get_houses(driver, ex_zip)
+# print(d_houses) 
+# display.stop()
 
 
 
