fde3bfee8384a07cbf85b42c044a35d7116ce930
Zak <elhjouji.zakaria@gmail.com>
2019-01-13 23:02:42 -0800

    pysetup

---
+++ lots-scraper-installation/py-setup.bat
@@ -0,0 +1,12 @@
+pip install pandas numpy lxml
+pip install beautifulsoup4
+pip install requests
+pip install progressbar2 geopy
+pip install selenium scipy termcolor
+pip install dash
+pip install dash_core_components dash_html_components
+echo 'cloning the directory ...'
+cd %USERPROFILE%\Desktop
+pause
+git clone https://github.com/zakariaelh/lots-scraper.git
+echo 'installation Done' 
